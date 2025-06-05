860bf5b715710d0eeeaa97075b3484c56543c570
ItzelAlvarez <itzel.alvarez4527@alumnos.udg.mx>
2025-06-05 12:25:28 -0600

    Agrega funcionalidad resta feat(Logo): Created a text logo for altatec with chatgpt but is not correct yet hola mundo testing style(textM): text add convertion function

---
+++ src/Convertion.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class Convertion
+    {
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+    }
+}
